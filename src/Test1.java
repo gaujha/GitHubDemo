42443345dcb7bafaabefa1b45f05462c0c2439b5
Gautam Jha <gautam.jha@abbott.com>
2021-06-08 10:22:49 +0530

    First Commit......

---
+++ src/Test1.java
@@ -0,0 +1,17 @@
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String hunger = "Yes";
+		
+		if(hunger == "Yes") {
+			System.out.println("Take a break");
+		}
+		else {
+			System.out.println("Don't Take a break");
+		}
+	}
+
+}
